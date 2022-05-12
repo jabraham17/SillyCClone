81083480701f78a1bd8040b07ecd215a01fc9bd2
Jacob Abraham <jacob.r.abraham@gmail.com>
2022-05-12 14:55:16 -0700

    improve cmdline, will add to other tools as well

--- src/tools/irdump/main.c
+++ src/tools/irdump/main.c
@@ -77,21 +77,67 @@ void dump_functions(FILE* fp, ir_function_list_t* funcs, int dumpSymbols) {
     }
 }
 
-int _debug_mode = 0;
+typedef struct ll_str ll_str_t;
+struct ll_str {
+    char* str;
+    ll_str_t* next;
+};
+ll_str_t* get_ll_str(char* n) {
+    ll_str_t* ll = malloc(sizeof(*ll));
+    ll->str = n;
+    ll->next = NULL;
+    return ll;
+}
+int ll_to_array(ll_str_t* ll, char*** array) {
+    int count;
+    ll_str_t* elm;
+    LL_COUNT(ll, elm, count);
 
+    if(count <= 0) {
+        *array = NULL;
+        return 0;
+    }
+    char** local_array = malloc(sizeof(*local_array) * count);
+    elm = ll;
+    int idx = 0;
+    while(idx < count && elm != NULL) {
+        local_array[idx] = elm->str;
+        idx++;
+        elm = elm->next;
+    }
+
+    *array = local_array;
+    return count;
+}
+
+int str_in_strarr(char* str, char** array, int length) {
+    for(int i = 0; i < length; i++) {
+        if(strcmp(str, array[i]) == 0) {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+int _debug_mode = 0;
 int main(int argc, char** argv) {
 
     char* inFileName = NULL;
     char* outFileName = NULL;
     int dumpSymbols = 0;
+    ll_str_t* functionNames_ll = NULL;
     char c;
     opterr = 1;
-    while((c = getopt(argc, argv, "i:o:sd")) != -1) {
+    while((c = getopt(argc, argv, "i:o:sf:d")) != -1) {
         switch(c) {
             case 'i': inFileName = optarg; break;
             case 'o': outFileName = optarg; break;
             case 'd': _debug_mode = 1; break;
             case 's': dumpSymbols = 1; break;
+            case 'f': {
+                LL_APPEND(functionNames_ll, get_ll_str(optarg));
+                break;
+            }
             case '?':
             default: return 1;
         }
@@ -106,14 +152,33 @@ int main(int argc, char** argv) {
     }
     if(!inFile) {
         fprintf(stderr, "Invalid input file\n");
-        return -1;
+        return 1;
     }
+
+    char** functionNames = NULL;
+    int nFunctions = ll_to_array(functionNames_ll, &functionNames);
 
     pt_t* root = parse(inFile);
     fclose(inFile);
     module_t* module = pt_to_ast(root);
     runPass(module, buildPass(PASS_build_symbol_table));
     ir_function_list_t* ir_funcs = ast_to_ir(module);
+
+    // only want to filter out after processing, before printing
+    // this way external references to other functions not included still work
+    if(nFunctions != 0) {
+        ir_function_list_t* filteredFuncs = NULL;
+        ir_function_list_t* elm = NULL;
+        ir_function_list_t* temp = NULL;
+        DL_FOREACH_SAFE(ir_funcs, elm, temp) {
+            if(str_in_strarr(
+                   elm->ir_function->name, functionNames, nFunctions)) {
+                DL_DELETE(ir_funcs, elm);
+                DL_APPEND(filteredFuncs, elm);
+            }
+        }
+        ir_funcs = filteredFuncs;
+    }
 
     FILE* outFile = NULL;
     if(outFileName != NULL) {
